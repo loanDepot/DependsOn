53178125b4e464ba6d0e5da2b712150e07732c1f
kevmar@gmail.com <KMarquette@loandepot.com>
2018-08-17 04:44:09 -0700

    fix syntax

--- BuildTasks/PublishModule.Task.ps1
+++ BuildTasks/PublishModule.Task.ps1
@@ -3,7 +3,7 @@ task PublishModule {
     if( $ENV:BHBuildSystem -ne 'Unknown' -and
         $ENV:BHBranchName -eq "master" -and
         [string]::IsNullOrWhiteSpace($ENV:APPVEYOR_PULL_REQUEST_NUMBER) -and
-        -not [string]::IsNullOrWhiteSpace(($ENV:NugetApiKey))
+        -not [string]::IsNullOrWhiteSpace($ENV:NugetApiKey))
     {
         Publish-Module -Path $Destination -NuGetApiKey $ENV:NugetApiKey -Repository $PSRepository -Force -Verbose
     }
